e2cc9bbc3d213e6eae3d96a7e43d1ee1c4843e93
rickyxu83 <rickyxu@gmail.com>
2026-08-10 01:47:37 +0800

    提升版本：管理端 26.0810.0146

--- frontend-admin/src/config/app.ts
+++ frontend-admin/src/config/app.ts
@@ -3,7 +3,7 @@ export const APP_NAME_HANT = "OMS Platform 運維智管"
 export const APP_VERSION = (
   (import.meta as any).env.VITE_APP_VERSION
   || (import.meta as any).env.VITE_APP_BUILD_VERSION
-  || "26.0810.0134"
+  || "26.0810.0146"
 
 )
 export const ADMIN_WORKSPACE_LABEL = "管理工作台"
